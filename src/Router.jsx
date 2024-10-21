58cfab783aac0095e6b4be9542b8ce19564a0c1b
seom98 <seo_m98@naver.com>
2024-10-21 22:55:17 +0900

    :sparkles: feat : Router.jsx 파일 추가 & WelcomePage 연결

---
+++ src/Router.jsx
@@ -0,0 +1,19 @@
+import React, { Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+
+const WelcomePage = React.lazy(() => import("./pages/WelcomePage"));
+
+export default function Router() {
+    return (
+        <BrowserRouter>
+            <Suspense fallback={<div>로딩중</div>}>
+                <Routes>
+                    <Route path="/portfolio" element={<App />}>
+                        <Route index element={<WelcomePage />} />
+                    </Route>
+                </Routes>
+            </Suspense>
+        </BrowserRouter>
+    );
+}
