c52f0271e12ebb01c3dad99241be90ce5c5f11b6
seom98 <seo_m98@naver.com>
2024-10-21 22:54:13 +0900

    :sparkles: feat : App.js -> App.jsx 변경 & Outlet태그 추가

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-    return <div>안녕하세요 만기페이지 입니다</div>;
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import { Outlet } from "react-router-dom";
+import "./App.css";
+
+export default function App() {
+    return <Outlet />;
+}
